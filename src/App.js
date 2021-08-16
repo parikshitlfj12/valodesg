7a3bd74064d0d0a42417edd268c8b2e4b61943df
Parikshit Singh <parikshitlfj@gmail.com>
2021-08-16 10:40:01 +0530

    Change Theme Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from "./components/header";
+import ThemeChanger from './components/ThemeChanger';
 import MainCards from './components/mainCards';
 import ThirdSec from './components/ThirdSec';
 import FourthSec from './components/FourthSec';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <ThemeChanger />
       <MainCards />
       <br />
       <small style={{color:"white", fontSize: "12px", fontWeight:"400"}}>Competitive: Standard VALORANT gameplay. Same rules as unrated, but at a higher stakes mode where you earn and compete for rank.</small>
---
+++ src/components/ThemeChanger.js
@@ -0,0 +1,26 @@
+import { React, useState } from "react";
+import {BiToggleRight, BiToggleLeft} from "react-icons/bi";
+import "../assets/styles/themeChanger.scss"
+
+function ThemeChanger() {
+  const [X, setX] = useState(true)
+  const [toggleIcon, setToggleIcon] = useState(<BiToggleLeft className="leftToggle"/>);
+  function changeToggle () {
+    console.log("Blick on x");
+    setX(!X);
+    setToggleIcon(X ? <BiToggleRight className="rightToggle"/> : <BiToggleLeft className="leftToggle" />);
+  }
+  return(
+    <div className="theme-changer">
+      <div className="inner-div">
+        <small>CHANGE THEME</small>
+        <div className="icons-style" onClick={changeToggle}>
+          {toggleIcon}
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default ThemeChanger;
