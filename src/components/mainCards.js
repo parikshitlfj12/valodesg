6a0dc853bf50f8cec8eb7e27f3592301000f2d77
Parikshit Singh <parikshitlfj@gmail.com>
2021-08-18 16:19:17 +0530

    Cards and buttons improved

--- src/components/mainCards.js
+++ src/components/mainCards.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap"
 import "../assets/styles/cardStyle.scss"
-import image from "../assets/img/Hivemind_Card.jpg"
+import imag1 from "../assets/img/12.png"
+import imag2 from "../assets/img/14.png"
+import imag3 from "../assets/img/2.png"
+import imag4 from "../assets/img/3.png"
+import imag5 from "../assets/img/4.png"
 
 function MainCards() {
-  var items = [1,2,3,4,5];
+  var items = [imag1, imag2, imag3, imag4, imag5];
   var list = items.map(item => {
     return(
       <div className="card-style">
-        <Card>
-          <Card.Img variant="top" src={image} />
-        </Card>
+        <img src={item} alt="Reyna" className="image-style"/>
       </div>
     )
   })
@@ -19,7 +20,6 @@ function MainCards() {
     <>
       <div className="center">
         {list}
-        
       </div>
     </>
   )
