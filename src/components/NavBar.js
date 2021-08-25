6795f037cbff27aeac7f25110ad99d0632adfde7
rajaabhaysingh <rajaabhaysingh233@gmail.com>
2021-08-25 14:01:05 +0530

    Navbar fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,38 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "../assets/styles/navBar.scss";
-import {NavLink} from "react-router-dom";
-import $ from 'jquery';
-import {BrowserRouter as Router, Route, useHistory} from "react-router-dom";
+import { NavLink, useRouteMatch } from "react-router-dom";
+import $ from "jquery";
+import { BrowserRouter as Router, Route, useHistory } from "react-router-dom";
 
-const NavBar = ({navItems}) => {
-  function animation(){
-    if(navItems){
-      var tabsNewAnim = $('#navbarSupportedContent');
-      var activeItemNewAnim = tabsNewAnim.find('.active');
+const NavBar = ({ navItems }) => {
+  const selectorNode = $(".hori-selector");
+
+  function animation() {
+    if (navItems && selectorNode && selectorNode.css) {
+      var tabsNewAnim = $("#navbarSupportedContent");
+      var activeItemNewAnim = tabsNewAnim.find(".active");
       var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
       var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
       var itemPosNewAnimTop = activeItemNewAnim.position();
       var itemPosNewAnimLeft = activeItemNewAnim.position();
       $(".hori-selector").css({
-        "top":itemPosNewAnimTop.top + "px", 
-        "left":itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
+        top: itemPosNewAnimTop.top + "px",
+        left: itemPosNewAnimLeft.left + "px",
+        height: activeWidthNewAnimHeight + "px",
+        width: activeWidthNewAnimWidth + "px",
       });
-      $("#navbarSupportedContent").on("click","li",function(e){
-        $('#navbarSupportedContent ul li').removeClass("active");
-        $(this).addClass('active');
+      $("#navbarSupportedContent").on("click", "li", function (e) {
+        $("#navbarSupportedContent ul li").removeClass("active");
+        $(this).addClass("active");
         var activeWidthNewAnimHeight = $(this).innerHeight();
         var activeWidthNewAnimWidth = $(this).innerWidth();
         var itemPosNewAnimTop = $(this).position();
         var itemPosNewAnimLeft = $(this).position();
         $(".hori-selector").css({
-          "top":itemPosNewAnimTop.top + "px", 
-          "left":itemPosNewAnimLeft.left + "px",
-          "height": activeWidthNewAnimHeight + "px",
-          "width": activeWidthNewAnimWidth + "px"
+          top: itemPosNewAnimTop.top + "px",
+          left: itemPosNewAnimLeft.left + "px",
+          height: activeWidthNewAnimHeight + "px",
+          width: activeWidthNewAnimWidth + "px",
         });
       });
     }
@@ -39,45 +41,33 @@ const NavBar = ({navItems}) => {
   useEffect(() => {
     animation();
   }, []);
-  
-  const history = useHistory();
-  const pathname = history.location.pathname;
-  
+
+  const { url } = useRouteMatch();
+
   let items;
-  if(navItems){
-    items = navItems.map(item => {
-      return(
-        <>
-          <li className={"nav-item " + (navItems[0] === item ? 'active' : '')}>
-            <NavLink to={pathname + "/" + item} exact>
-              <i 
-              className="fas fa-tachometer-alt">
-              </i>{item}
-            </NavLink>
-          </li>
-        </>
-      )
-    })
+  if (navItems) {
+    items = navItems.map((item) => {
+      return (
+        <li className={"nav-item " + (navItems[0] === item ? "active" : "")}>
+          <NavLink to={url + "/" + item}>
+            <i className="fas fa-tachometer-alt"></i>
+            {item}
+          </NavLink>
+        </li>
+      );
+    });
   }
-  
+
   return (
     <nav className="navbar navbar-expand-lg navbar-mainbg">
-      <Router>
-        <Route path="/">
-          <div id="navbarSupportedContent">
-            <ul>
-              {/* The white Animation Part */}
-              <div className="hori-selector"></div>
-              
-              {items}
+      <div id="navbarSupportedContent">
+        <ul>
+          {/* The white Animation Part */}
+          <div className="hori-selector"></div>
 
+          {items}
 
-
-
-
-
-
-               {/* <li className="nav-item active">
+          {/* <li className="nav-item active">
                 <NavLink to="/" exact>
                   <i 
                   className="fas fa-tachometer-alt">
@@ -85,7 +75,7 @@ const NavBar = ({navItems}) => {
                 </NavLink>
               </li> */}
 
-              {/*<li className="nav-item">
+          {/*<li className="nav-item">
                 <NavLink to="/about" exact>
                   <i 
                   className="far fa-address-book">
@@ -121,12 +111,10 @@ const NavBar = ({navItems}) => {
                   </i>CUSTOM
                 </NavLink>
               </li> */}
-            </ul>
-          </div>
-        </Route>
-      </Router>
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default NavBar;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,6 @@
-import NavBar from "../components/NavBar"
-import ThemeChanger from "../components/ThemeChanger"
-import { BrowserRouter as Router, Route, Switch, useHistory } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import ThemeChanger from "../components/ThemeChanger";
+import { Route, Switch, useRouteMatch, Redirect } from "react-router-dom";
 
 // Importing Components
 import OneProject from "../components/OneProject";
@@ -8,21 +8,33 @@ import HappyFarm from "../components/projects/HappyFarm";
 import imageHappyFarm from "../assets/img/usr.png";
 import Portfolio from "../components/projects/Portfolio";
 import imagePortfolio from "../assets/img/Screenshot (471).png";
-import WilsWork from "../components/projects/WilsWork"
-import imageWils from "../assets/img/Screenshot (472).png"
+import WilsWork from "../components/projects/WilsWork";
+import imageWils from "../assets/img/Screenshot (472).png";
 
-export default function Projects () {
-  const items = ["Happy Farm", "Portfolio", "WilsWork", "Trading app", "Apis"];
+export default function Projects() {
+  const items = ["HappyFarm", "Portfolio", "WilsWork", "TradingApp", "Apis"];
 
-  return(
+  const { path } = useRouteMatch();
+
+  return (
     <>
-      <NavBar navItems={items}/>
+      <NavBar navItems={items} />
       <ThemeChanger />
-      {/* <OneProject Content={HappyFarm} image={imageHappyFarm}/>
-      <OneProject Content={Portfolio} image={imagePortfolio}/> */}
-      <OneProject Content={WilsWork} image={imageWils}/>
 
+      <Switch>
+        <Route exact path={`${path}`}>
+          <Redirect to={`${path}/HappyFarm`} />
+        </Route>
+        <Route path={`${path}/HappyFarm`}>
+          <OneProject Content={HappyFarm} image={imageHappyFarm} />
+        </Route>
+        <Route path={`${path}/Portfolio`}>
+          <OneProject Content={Portfolio} image={imagePortfolio} />
+        </Route>
+        <Route path={`${path}/WilsWork`}>
+          <OneProject Content={WilsWork} image={imageWils} />
+        </Route>
+      </Switch>
     </>
-  )
+  );
 }
-
